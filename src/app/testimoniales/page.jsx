4f3e845da5f5dec2262bf9cd485614d7af0aaf3d
ezelujan2 <eze.rosario2000@gmail.com>
2023-08-30 15:31:14 -0300

    Agregado de rutas,estilos

--- src/app/testimoniales/page.jsx
+++ src/app/testimoniales/page.jsx
@@ -1,5 +1,54 @@
+import { Rubik, Poppins } from "next/font/google";
+import TestCard from "@/components/TestCard";
+
+const fontRubik = Rubik({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const fontPoppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+const poppins = fontPoppins.className;
+const rubik = fontRubik.className;
+
+const testimonio = {
+  name: "Juanito Perez",
+  profession: "Abogado",
+  rating: 4,
+  image:
+    "https://img.freepik.com/foto-gratis/chico-worldface-espanol-fondo-blanco_53876-137665.jpg?w=360",
+  description:
+    "Nunca habiamos rentado un auto antes, pero luego de haber puesto nuestro voto de confianza en esta pagina es una experiencia que quiero repetir cada que tenga vacaciones",
+};
+
+const arr = [1, 2, 3, 4];
+
 function Testimoniales() {
-  return <div>Testimoniales</div>;
+  return (
+    <div>
+      <div>
+        <h2 className={`${poppins} text-4xl p-10 bg-gray-400 sm:text-6xl`}>
+          Testimoniales
+        </h2>
+      </div>
+      <div>
+        {arr.map((numero) => {
+          return (
+            <TestCard
+              key={testimonio.name}
+              name={testimonio.name}
+              profession={testimonio.profession}
+              rating={testimonio.rating}
+              description={testimonio.description}
+              image={testimonio.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Testimoniales;
---
+++ src/components/TestCard.jsx
@@ -0,0 +1,47 @@
+import { Rubik, Poppins } from "next/font/google";
+
+const fontRubik = Rubik({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const fontPoppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+const poppins = fontPoppins.className;
+const rubik = fontRubik.className;
+
+function TestCard({ name, description, profession, rating, image }) {
+  return (
+    <div className="p-4 my-4 border border-gray-300 rounded-lg shadow-md mx-20 bg-gris_fondo">
+      <h4 className={`text-sm mb-2  italic ${poppins}`}>{description}</h4>
+      <div className="flex items-center space-x-3">
+        <div className="w-12 h-12 overflow-hidden rounded-full">
+          <img className="object-cover w-full h-full" src={image} alt={name} />
+        </div>
+        <div>
+          <h4 className="text-base font-medium">{name}</h4>
+          <p className="text-sm text-gray-500">{profession}</p>
+        </div>
+        <div className="flex items-center text-yellow-400 space-x-1 mr-0">
+          {Array.from({ length: rating }).map((_, index) => (
+            <svg
+              key={index}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              className="w-5 h-5">
+              <path
+                fillRule="evenodd"
+                d="M10 1a1 1 0 0 1 .95.684l1.5 4.79a1 1 0 0 0 .949.686l4.386.011a1 1 0 0 1 .55 1.73L14.35 10.59a1 1 0 0 0-.29 1.05l1.436 4.643a1 1 0 0 1-1.548 1.14L10 15.63a1 1 0 0 0-.78 0l-4.388 2.243a1 1 0 0 1-1.548-1.14L5.94 11.64a1 1 0 0 0-.29-1.05L1.113 7.427a1 1 0 0 1 .55-1.73l4.386-.01a1 1 0 0 0 .95-.684l1.5-4.79A1 1 0 0 1 10 1z"
+              />
+            </svg>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestCard;
