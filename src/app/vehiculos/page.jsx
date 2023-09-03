9da6c1f1c28ae36a662f4cc47043c8d10795950f
Christian Araneda <ignacio.aranedaocares@gmail.com>
2023-09-03 13:31:54 -0300

    Configuracion de vehiculos y filtro

--- src/app/vehiculos/page.jsx
+++ src/app/vehiculos/page.jsx
@@ -1,13 +1,10 @@
+"use client";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Rubik, Poppins } from "next/font/google";
-import axios from "axios";
-import {
-  BsChevronCompactRight,
-  BsCheckCircleFill,
-  BsFillCreditCard2BackFill,
-} from "react-icons/bs";
-
-import { categorias } from "../../libs/categorias.js";
+import FiltroVehiculos from "@/components/FiltroVehiculos";
 import CarCard from "@/components/CarCard.jsx";
+import { getCars } from "@/store/slices/car";
 
 const fontRubik = Rubik({
   weight: "600",
@@ -21,15 +18,15 @@ const fontPoppins = Poppins({
 const poppins = fontPoppins.className;
 const rubik = fontRubik.className;
 
-async function loadProducts() {
-  const { data } = await axios.get("http://localhost:3000/api/products");
-  return data;
-  // const result=await conn.query('SELECT * FROM product')
-  // console.log(result);
-}
+function Vehiculos() {
+  const dispatch = useDispatch();
 
-async function vehiculos() {
-  const products = await loadProducts();
+  useEffect(() => {
+    dispatch(getCars());
+  }, []);
+
+  const cars = useSelector((state) => state.cars.showCars);
+
   return (
     <>
       <header
@@ -44,58 +41,13 @@ async function vehiculos() {
         <p className={`text-[1rem] ${rubik} mb-2 text-center`}>
           Encuentra el vehículo ideal
         </p>
-        <form
-          className={`pt-2 ${rubik} text-[0.8em] bg-negro_fondo pb-5 rounded-xl flex w-[95%] mx-auto pl-4 justify-center items-center`}>
-          <div className={`flex flex-col mr-10 w-[20%]`}>
-            <label htmlFor="busqueda" className={`text-[1rem] text-white mb-1`}>
-              Búsqueda
-            </label>
-            <input
-              type="text"
-              className={`bg-gris_fondo min-w-[80px] w-[90%] rounded px-2 py-1`}
-            />
-          </div>
-          <div className={`flex flex-col mr-10 w-[20%]`}>
-            <label
-              htmlFor="categoria"
-              className={`text-[1rem] text-white mb-1`}>
-              Categoría
-            </label>
-            <select
-              className={`bg-gris_fondo min-w-[80px] w-[90%] rounded px-2 py-1`}
-              name="categoría">
-              <option selected className="">
-                {" "}
-              </option>
-              {categorias.map((c) => (
-                <option key={c.tipo}>{c.tipo}</option>
-              ))}
-            </select>
-          </div>
-          <div className={`flex flex-col mr-10 w-[20%]`}>
-            <label
-              htmlFor="categoria"
-              className={`text-[1rem] text-white mb-1`}>
-              Capacidad
-            </label>
-            <select
-              className={`bg-gris_fondo min-w-[80px] w-[90%] rounded px-2 py-1`}
-              name="capacidad">
-              <option selected className="">
-                {" "}
-              </option>
-              {categorias.map((c) => (
-                <option key="categoria">{c.tipo}</option>
-              ))}
-            </select>
-          </div>
-        </form>
+        <FiltroVehiculos />
       </section>
       <section
         className={`pt-4 ${rubik} mx-[auto] text-[0.8em] bg-gris_frente pb-12 place-items-center w-[95%]`}>
-        <div className="  grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-2 gap-y-10">
-          {products.map((product) => (
-            <CarCard key={product.id} product={product} />
+        <div className="  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-2 gap-y-10">
+          {cars?.map((car) => (
+            <CarCard key={car.id} car={car} />
           ))}
         </div>
       </section>
@@ -103,4 +55,4 @@ async function vehiculos() {
   );
 }
 
-export default vehiculos;
+export default Vehiculos;
---
+++ src/components/FiltroVehiculos.jsx
@@ -0,0 +1,121 @@
+"use client";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setShowCars } from "@/store/slices/car";
+import { MultiSelect } from "react-multi-select-component";
+import { categorias, modelos, capacidades } from "../libs/categorias";
+
+const optionsCategorias = categorias.map((categoria) => ({
+  label: categoria.tipo,
+  value: categoria.tipo,
+}));
+
+const optionsModelos = modelos.map((modelo) => ({
+  label: modelo.tipo,
+  value: modelo.tipo,
+}));
+
+const optionsCapacidades = capacidades.map((capacidad) => ({
+  label: capacidad.capacity,
+  value: capacidad.capacity,
+}));
+
+export default function FiltroVehiculos() {
+  const dispatch = useDispatch();
+
+  const [modelo, setModelo] = useState([]);
+  const [categoria, setCategoria] = useState([]);
+  const [capacidad, setCapacidad] = useState([]);
+
+  useEffect(() => {
+    dispatch(setShowCars({ modelo, categoria, capacidad }));
+  }, [modelo, categoria, capacidad]);
+
+  return (
+    <form
+      className={`pt-2 text-[0.8em] bg-negro_fondo pb-5 rounded-xl  w-[95%] mx-auto pl-4 justify-center items-center space-y-2 sm:space-y-0 sm:flex sm:flex-wrap`}>
+      <div className={`flex  flex-col  sm:w-[20%] w-full`}>
+        <label htmlFor="busqueda" className={`text-[1rem] text-white mb-1`}>
+          Búsqueda
+        </label>
+        <input
+          type="text"
+          className={`bg-white min-w-[80px] w-[95%] rounded h-10  p-2 text-black text-xs`}
+          placeholder="Nombre Vehiculo"
+        />
+      </div>
+      <div className={`flex flex-col sm:w-[20%] w-full`}>
+        <label htmlFor="Modelo" className={`text-[1rem] text-white mb-1`}>
+          Modelo
+        </label>
+
+        <MultiSelect
+          options={optionsModelos}
+          value={modelo}
+          onChange={setModelo}
+          labelledBy="Modelo"
+          className={` min-w-[80px] max-w-[95%] rounded py-1`}
+          overrideStrings={{
+            allItemsAreSelected: "Todos los modelos",
+            clearSearch: "Limpiar busqueda",
+            clearSelected: "Limpiar seleccion",
+            noOptions: "No hay opciones",
+            search: "Buscar",
+            selectAll: "Seleccionar Todo",
+            selectAllFiltered: "Select All (Filtered)",
+            selectSomeItems: "Seleccionar...",
+            create: "Crear",
+          }}
+        />
+      </div>
+      <div className={`flex flex-col sm:w-[20%] w-full`}>
+        <label htmlFor="categoria" className={`text-[1rem] text-white mb-1`}>
+          Categoría
+        </label>
+
+        <MultiSelect
+          options={optionsCategorias}
+          value={categoria}
+          onChange={setCategoria}
+          labelledBy="Select"
+          className={` min-w-[80px] max-w-[95%] rounded py-1`}
+          overrideStrings={{
+            allItemsAreSelected: "Todas las categorias",
+            clearSearch: "Limpiar busqueda",
+            clearSelected: "Limpiar seleccion",
+            noOptions: "No hay opciones",
+            search: "Buscar",
+            selectAll: "Seleccionar Todo",
+            selectAllFiltered: "Select All (Filtered)",
+            selectSomeItems: "Seleccionar...",
+            create: "Crear",
+          }}
+        />
+      </div>
+      <div className={`flex flex-col  sm:w-[20%] w-full`}>
+        <label htmlFor="categoria" className={`text-[1rem] text-white mb-1`}>
+          Capacidad
+        </label>
+        <MultiSelect
+          options={optionsCapacidades}
+          value={capacidad}
+          onChange={setCapacidad}
+          labelledBy="Capacidad"
+          className={` min-w-[80px] max-w-[95%] rounded py-1`}
+          overrideStrings={{
+            allItemsAreSelected: "Todas las capacidades",
+            clearSearch: "Limpiar busqueda",
+            clearSelected: "Limpiar seleccion",
+            noOptions: "No hay opciones",
+            search: "Buscar",
+            selectAll: "Seleccionar Todo",
+            selectAllFiltered: "Select All (Filtered)",
+            selectSomeItems: "Seleccionar...",
+            create: "Crear",
+          }}
+        />
+      </div>
+    </form>
+  );
+}
