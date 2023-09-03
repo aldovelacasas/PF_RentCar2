66f52e0d9ac43c13705a30fce700ac997d2573cc
Christian Araneda <ignacio.aranedaocares@gmail.com>
2023-09-03 13:17:54 -0300

    Configuracion redux

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,9 @@
+import { Providers } from "../store/providers";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/footer";
+import { store } from "@/store";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="">{children}</div>
-        <Footer />
+        <Providers store={store}>
+          <Navbar />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
