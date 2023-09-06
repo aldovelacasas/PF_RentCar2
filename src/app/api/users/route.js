cf61752021cce95c55301e69123218d76a498c56
tonyzl <germanantoniosiete@gmail.com>
2023-09-06 18:07:24 -0500

    creacion de ruta para editar usuario

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,11 +1,30 @@
 import { NextResponse } from "next/server";
 import { conn } from "@/libs/mysql";
 
+export async function GET(){
+  try {
+      const results = await conn.query("SELECT * FROM user");
+      return NextResponse.json(results)
+      
+  } catch (error) {
+      return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+          status:500,
+      }
+
+      );
+  }
+}
+
+
 export async function POST(request) {
   try {
     const { uid, emailUser } = await request.json();
 
-    const result = await conn.query("INSERT INTO usuarios_firebase SET ?", {
+    const result = await conn.query("INSERT INTO user SET ?", {
       uid,
       emailUser,
     });
