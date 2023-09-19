2aee4cc813aed9779a6a017c1e447caabe2d28bb
Christian Araneda <ignacio.aranedaocares@gmail.com>
2023-09-19 00:41:09 -0300

    Chat admin y user

--- src/components/ChatClient.jsx
+++ src/components/ChatClient.jsx
@@ -4,12 +4,36 @@ function ChatClient(props) {
     console.log("miraaaaa", message);
     console.log(message);
     return (
-      <div key={index}>
-        <div>
-          <h4>{message.sender}</h4>
+      <div
+        key={index}
+        className={`flex flex-col px-2 py-4 items-${
+          message.sender === "AutoConnect" ? "start" : "end"
+        }`}>
+        <div className="flex flex-col">
+          <h4
+            className={`font-bold ${
+              message.sender === "AutoConnect"
+                ? "text-orange-500 text-right"
+                : "text-blue-500 text-left"
+            }`}>
+            {message.sender}
+          </h4>
         </div>
-        <div>
-          <p>{message.content}</p>
+        <div className="flex">
+          <div
+            className={`relative ${
+              message.sender === "AutoConnect" ? "items-start" : "items-end"
+            }`}>
+            <p className="bg-white p-3 rounded-lg shadow-md relative">
+              {message.content}
+            </p>
+            <div
+              className={`w-4 h-4 bg-white absolute top-1/2 ${
+                message.sender === "AutoConnect"
+                  ? "left-[-8px]"
+                  : "right-[-8px]"
+              } transform -translate-y-1/2  bg-white rotate-45 z-0`}></div>
+          </div>
         </div>
       </div>
     );
@@ -17,7 +41,7 @@ function ChatClient(props) {
 
   let body;
 
-  body = <div name="messages">{props.messages?.map(renderMessages)}</div>;
+  body = <>{props.messages?.map(renderMessages)}</>;
 
   function handleKeyPress(e) {
     if (e.key === "Enter") {
@@ -26,10 +50,11 @@ function ChatClient(props) {
   }
 
   return (
-    <div name="container">
-      <div name="chatPanel">
-        <div name="bodyContainer">{body}</div>
+    <div className="">
+      <div className="">
+        <div className="">{body}</div>
         <textarea
+          className="resize-none"
           id="textbox"
           value={props.message}
           onChange={props.handleMessageChange}
